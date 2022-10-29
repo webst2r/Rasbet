4cd8d69603f9e4901955cddc17d84ac64789219d
Cláudia <claudiadm.ribeiro@hotmail.com>
2022-10-29 15:48:27 +0100

    added repositories

--- backend/src/main/java/pt/rasbet/backend/repository/ApostaMultiplaRepository.java
+++ backend/src/main/java/pt/rasbet/backend/repository/ApostaMultiplaRepository.java
@@ -1,4 +1,22 @@
 package pt.rasbet.backend.repository;
 
-public interface ApostaMultiplaRepository {
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import pt.rasbet.backend.entity.ApostasMultiplas;
+import pt.rasbet.backend.entity.QApostasMultiplas;
+
+@RepositoryRestResource(collectionResourceRel = "apostaMultipla", path = "apostaMultipla")
+public interface ApostaMultiplaRepository extends JpaRepository<ApostasMultiplas, Long>, QuerydslPredicateExecutor<ApostasMultiplas>, QuerydslBinderCustomizer<QApostasMultiplas> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QApostasMultiplas qApostasMultiplas) {
+        // Make case-insensitive 'like' filter for all string properties
+        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 }
--- backend/src/main/java/pt/rasbet/backend/repository/TipoRepository.java
+++ backend/src/main/java/pt/rasbet/backend/repository/TipoRepository.java
@@ -1,4 +1,22 @@
 package pt.rasbet.backend.repository;
 
-public interface TipoRepository {
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import pt.rasbet.backend.entity.Tipo;
+import pt.rasbet.backend.entity.QTipo;
+
+@RepositoryRestResource(collectionResourceRel = "tipo", path = "tipo")
+public interface TipoRepository extends JpaRepository<Tipo, Long>, QuerydslPredicateExecutor<Tipo>, QuerydslBinderCustomizer<QTipo> {
+
+@Override
+default void customize(QuerydslBindings bindings, QTipo qTipo) {
+        // Make case-insensitive 'like' filter for all string properties
+        bindings.bind(String.class).first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+        }
 }
