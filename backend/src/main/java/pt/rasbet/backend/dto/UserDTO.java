f3a28dd700c7ca3570c2ce23aa52b246479e3fa3
Cláudia <claudiadm.ribeiro@hotmail.com>
2022-10-29 11:12:44 +0100

    added birthdate

--- backend/src/main/java/pt/rasbet/backend/dto/UserDTO.java
+++ backend/src/main/java/pt/rasbet/backend/dto/UserDTO.java
@@ -1,14 +1,18 @@
 package pt.rasbet.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 import pt.rasbet.backend.utils.ExceptionConstants;
 import pt.rasbet.backend.entity.User;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+
+import java.time.LocalDate;
 
 import static pt.rasbet.backend.utils.Constants.*;
 
@@ -26,11 +30,14 @@ public class UserDTO {
     @NotBlank(message = "password " + ExceptionConstants.REQUIRED)
     private String password;
 
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
     @NotBlank(message = "role " + ExceptionConstants.REQUIRED)
     @Pattern(regexp = "^(" + ROLE_USER + "|" + ROLE_SPECIALIST + "|" + ROLE_ADMIN + ")$", message = ExceptionConstants.ROLE_NOT_EXISTS)
     private String role;
 
     public User toEntity() {
-        return new User(this.firstName, this.lastName, this.email, this.password);
+        return new User(this.firstName, this.lastName, this.email, this.password, this.birthDate);
     }
 }
